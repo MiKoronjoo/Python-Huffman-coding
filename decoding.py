835f15589ffbe3abe3677c79ed6d0da45f264ccc
Hassan <hassan.abbp@gmail.com>
2019-06-02 15:32:31 +0430

    Decoding zip file

---
+++ decoding.py
@@ -0,0 +1,72 @@
+# code_text = ''
+import sys
+
+code_dic = {}
+
+
+def generate_dic(file_name):
+    file_stream = open(file_name, 'r')
+    for line in file_stream.readlines():
+        char, _, code = line.split('\t')
+        if char == '\\n':
+            char = '\n'
+        elif char == '\\t':
+            char = '\t'
+        elif char == '\\r':
+            char = '\r'
+        code_dic.update({code.strip(): char})
+
+
+def decoding(bin_text):
+    temp_code = ''
+    decode_text = ''
+    for b in bin_text:
+        temp_code += b
+        if temp_code in code_dic:
+            char = code_dic[temp_code]
+            if char == '\0':
+                break
+            decode_text += char
+            temp_code = ''
+
+    return decode_text
+
+
+def main(zip_address: str, huffman_address: str) -> None:
+    try:
+        file_stream = open(zip_address, 'r')
+        code_text = file_stream.read()
+        file_stream.close()
+    except FileNotFoundError as ex:
+        print('No such file or directory:', ex.filename)
+        return
+
+    try:
+        generate_dic(huffman_address)
+    except FileNotFoundError as ex:
+        print('No such file or directory:', ex.filename)
+        return
+
+    bin_text = ''
+    for char in code_text:
+        temp_code = bin(ord(char))[2:]
+        if len(temp_code) < 8:
+            temp_code = (8 - len(temp_code)) * '0' + temp_code
+        bin_text += temp_code
+
+    decode_text = decoding(bin_text)
+
+    file_stream = open('Output.txt', 'w')
+    file_stream.write(decode_text)
+    file_stream.close()
+
+    print("File '%s' unzipped to 'Output.txt' successfully\n" % zip_address)
+
+
+if __name__ == '__main__':
+    try:
+        zip_address, huffman_address = sys.argv[1], sys.argv[2]
+    except IndexError:
+        zip_address, huffman_address = 'Zip.txt', 'Huffman.txt'
+
+    main(zip_address, huffman_address)
